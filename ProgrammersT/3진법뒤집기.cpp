010307959773f22b95cc589c198e68e88e52f2d9
LeeYuHwan <dbghksskfk@naver.com>
2020-10-19 04:39:29 +0900

    AlgorithmQuiz

---
+++ ProgrammersT/3진법뒤집기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    string tmp = "";
+    int num = 0;
+
+    while (n >= 3) {
+        num = n % 3;
+        n /= 3;
+
+        tmp = to_string(num) + tmp;
+    }
+
+    tmp = to_string(n) + tmp;
+
+    for (int i = 0; i < tmp.length(); i++) {
+        answer += (tmp[i] - '0') * pow(3, i);
+    }
+    return answer;
+}
