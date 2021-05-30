41530320fa21046027ecb0cbcac2022ed55140d1
LeeYuHwan <dbghksskfk@naver.com>
2021-05-30 17:57:07 +0900

    Edit Greedy

--- BeakJoonT/우체국.cpp
+++ BeakJoonT/우체국.cpp
@@ -4,11 +4,12 @@
 using namespace std;
 
 int main()
-{	
+{
 	ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
 	int N = 0;
 	cin >> N;
-	
+
 	vector<pair<long long, long long>> v;
 	long long sum = 0;
 	for (int i = 0; i < N; i++) {
@@ -19,18 +20,15 @@ int main()
 	}
 
 	sort(v.begin(), v.end());
-    sum += 1;
+	sum += 1;
 	sum /= 2;
 
-    int answer = 0;
 	long long tmp = 0;
-	for (int i = 0; i < N; i++) {
-		tmp += v[i].second;
-		if (tmp >= sum) {
-			answer = v[i].first;
-			break;
-		}
+	int idx = 0;
+	while (tmp < sum) {
+		tmp += v[idx].second;
+		idx++;
 	}
-    
-    cout << answer;
+
+	cout << v[idx - 1].first;
 }
