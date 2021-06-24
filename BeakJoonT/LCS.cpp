735243342e6779da0b40baf737fd1b7296da31be
LeeYuHwan <dbghksskfk@naver.com>
2021-06-25 03:24:37 +0900

    DP

---
+++ BeakJoonT/LCS.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+	string str1 = "";
+	string str2 = "";
+	cin >> str1 >> str2;
+
+	vector<vector<int>> dp(1001, vector<int>(1001, 0));
+
+	for (int i = 1; i <= str1.length(); i++) {
+		for (int j = 1; j <= str2.length(); j++) {
+			if (str1[i - 1] == str2[j - 1]) {
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			}
+			else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+		}
+	}
+
+	cout << dp[str1.length()][str2.length()];
+}
