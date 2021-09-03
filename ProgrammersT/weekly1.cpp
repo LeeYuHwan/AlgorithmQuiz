9d6274d2c8016d6dc1ed981190a77c9f49377e16
LeeYuHwan <dbghksskfk@naver.com>
2021-09-04 00:45:31 +0900

    Weekly1

---
+++ ProgrammersT/weekly1.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long answer = money;
+
+    for (int i = 1; i <= count; i++) {
+        answer -= (price * i);
+    }
+
+    if (answer >= 0) answer = 0;
+    else answer *= -1;
+
+    return answer;
+}
