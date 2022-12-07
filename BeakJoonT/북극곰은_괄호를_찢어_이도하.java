354fd10c0b79a9f9c7da7f4d685b83666fa5be37
LeeYuHwan <66478929+LeeYuHwan@users.noreply.github.com>
2022-12-07 15:11:48 +0900

    Greedy
    commit name change

--- BeakJoonT/북극곰은_괄호를_찢어_이도하.java
+++ BeakJoonT/북극곰은_괄호를_찢어_이도하.java
@@ -5,6 +5,7 @@ import java.util.StringTokenizer;
 
 public class Q25918 {
     public static void main(String[] args) throws IOException {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
